504be4c192035a24e687ff989169afee0f28b3fb
Nhiên Ngô Văn Đức <nhienhk19@gmail.com>
2023-11-01 13:23:47 +0700

    1/11/23

--- bai8/file_posteddata.php
+++ bai8/file_posteddata.php
@@ -1,62 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <title>Sample &mdash; CKEditor</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link type="text/css" rel="stylesheet" href="sample.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CKEditor Example</title>
+    <script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script> <!-- Thêm đường dẫn CDN của CKEditor -->
+
 </head>
 
 <body>
-    <h1 class="samples">
-        CKEditor &mdash; Posted Data</h1>
-    <table border="1" cellspacing="0" id="outputSample">
-        <colgroup>
-            <col width="100" />
-        </colgroup>
-        <thead>
-            <tr>
-                <th>Field&nbsp;Name</th>
-                <th>Value</th>
-            </tr>
-        </thead>
-        <?php
-        if (isset($_POST))
-            $postArray = &$_POST; // 4.1.0 or later, use $_POST
-        else
-            $postArray = &$HTTP_POST_VARS; // prior to 4.1.0, use HTTP_POST_VARS
-        foreach ($postArray as $sForm => $value) {
-            if (get_magic_quotes_gpc())
-                $postedValue = htmlspecialchars(stripslashes($value));
-            else
-                $postedValue = htmlspecialchars($value);
-        ?>
-            <tr>
-                <th style="vertical-align: top"><?php echo htmlspecialchars($sForm);
-                                                ?></th>
-                <td>
-                    <pre class="samples"><?php echo $postedValue ?></pre>
-                </td>
-            </tr>
-        <?php
-        }
-        ?>
-    </table>
-    <div id="footer">
-        <hr />
-        <p>
-            CKEditor - The text editor for the Internet - <a class="samples" href="http://ckeditor.com/">http://ckeditor.com</a>
-        </p>
-        <p id="copy">
-            Copyright &copy; 2003-2011, <a class="samples" href="http://cksource.com/">CKSource</a> - Frederico Knabben. All rights reserved.
-        </p>
-    </div>
+    <textarea name="editor1"></textarea> <!-- Tạo một vùng text area với tên "editor1" -->
+    <script>
+        CKEDITOR.replace('editor1'); // Khởi tạo CKEditor cho vùng text area có tên "editor1"
+    </script>
 </body>
 
 </html>
